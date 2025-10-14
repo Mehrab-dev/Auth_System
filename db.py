031975d8fe199d9a65c9df315fce24224850b66b
mehrab khanmohammadi <programmer.py.mail@gmail.com>
2025-10-15 00:37:37 +0330

    create class User id models file and create database in db.py

--- db.py
+++ db.py
@@ -1,1 +1,8 @@
-ECHO is on.
+import sqlite3
+
+def init_db() :
+    with sqlite3.connect('Security_system.sqlite3') as conn :
+        cur = conn.cursor()
+        cur.execute(""" CREATE TABLE if not exists users(id TEXT PRIMARY KEY, name TEXT , lastname TEXT , phone TEXT , email TEXT ,
+                        password TEXT , membership_date TEXT , last_visit TEXT) """)
+        conn.commit()
--- models.py
+++ models.py
@@ -1,1 +1,24 @@
-ECHO is on.
+import re
+
+
+class User :
+    def __init__(self,name:str,lastname:str,phone:str,password:str,email:str=None) :
+        self.name = name
+        self.lastname = lastname
+        match = re.fullmatch(r"\d{11}",phone)
+        if not match :
+            raise ValueError("The phone number is invalid")
+        else :
+            self.phone = phone
+        match = re.fullmatch(r"\d^{8}",password)
+        if not match :
+            raise ValueError("The password is invalid")
+        else :
+            self.password = password
+        if email is not None :
+            match = re.fullmatch(r"^[a-zA-Z0-9._]+@gmail\.com$",email)
+            if not match :
+                raise ValueError("The email is invalid")
+            else :
+                self.email = email
+
